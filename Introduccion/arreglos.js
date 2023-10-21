5d5ec792163af9e4086b74acb688b29cef4a7360
adeo7 <bdmosquera7@misena.edu.co>
2023-10-21 16:53:36 -0500

    estoy uniendo la parte de los arreglos

--- Introduccion/arreglos.js
+++ Introduccion/arreglos.js
@@ -1,1 +1,17 @@
- 
+// se pueden guardar arreglos dentro de los arreglos
+const arreglo = ['texto', 456.10, false, { propiedad: "valor" }, [1, 2, 3]];
+console.log(arreglo);
+
+const amigos = ['alejandro', 'manuel', 'cesar'];
+console.log(amigos[1]);
+
+const colores = [];
+colores [0] = 'rojo';
+colores [1] = 'verde';
+
+// para ver cuantos objetos hay en el array se usa length
+console.log('el arreglo colores tiene:' + colores.length  +  ' colores');
+
+// push
+colores.push('azul');
+console.log(colores); 
